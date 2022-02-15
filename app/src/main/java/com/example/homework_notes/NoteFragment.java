0d030c45cb55826c7eefc4847bd70562fcb1adee
JaneNaumova <zhenka_umka@mail.ru>
2022-02-15 16:09:59 +0300

    Create fragments

---
+++ app/src/main/java/com/example/homework_notes/NoteFragment.java
@@ -0,0 +1,31 @@
+package com.example.homework_notes;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class NoteFragment extends Fragment {
+
+    public static NoteFragment newInstance() {
+        NoteFragment fragment = new NoteFragment();
+        return fragment;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_note, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/example/homework_notes/NoteListFragment.java
@@ -0,0 +1,32 @@
+package com.example.homework_notes;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NoteListFragment extends Fragment {
+
+    public static NoteListFragment newInstance() {
+        NoteListFragment fragment = new NoteListFragment();
+        return fragment;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_note_list, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
