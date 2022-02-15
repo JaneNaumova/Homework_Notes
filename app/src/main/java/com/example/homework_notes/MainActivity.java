f916a35ebda219bde53977f702583af5ddc798d5
JaneNaumova <zhenka_umka@mail.ru>
2022-02-15 17:15:45 +0300

    create the base

--- app/src/main/java/com/example/homework_notes/MainActivity.java
+++ app/src/main/java/com/example/homework_notes/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.homework_notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +12,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        if(savedInstanceState==null){
+            NoteListFragment noteListFragment = new NoteListFragment();
+            getSupportFragmentManager().beginTransaction().replace(R.id.list,noteListFragment).commit();
+            if(getResources().getConfiguration().orientation== Configuration.ORIENTATION_LANDSCAPE){
+                NoteFragment noteFragment = new NoteFragment();
+                getSupportFragmentManager().beginTransaction().replace(R.id.notes,noteFragment).commit();
+            }
+        }
     }
 }
